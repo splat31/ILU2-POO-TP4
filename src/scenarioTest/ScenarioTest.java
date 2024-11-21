71b7e93f73dfc5f79e6ffe680a18c1fc7396c4eb
chaudet <christelle.chaudet@irit.fr>
2024-11-21 09:53:06 +0100

    Update ScenarioTest.java

--- src/scenarioTest/ScenarioTest.java
+++ src/scenarioTest/ScenarioTest.java
@@ -4,23 +4,23 @@ import villagegaulois.Etal;
 
 public class ScenarioTest {
 
-	public static void acheterProduit(Etal[] marche, String produit, int quantiteSouhaitee) {
-		int quantiteRestante = quantiteSouhaitee;
-		for (int i = 0; i < marche.length && quantiteRestante != 0; i++) {
-			Etal etal = marche[i];
-			int quantiteDisponible = etal.contientProduit(produit, quantiteRestante);
-			if (quantiteDisponible != 0) {
-				int prix = etal.acheterProduit(quantiteDisponible);
-				String chaineProduit = accorderNomProduit(produit, quantiteDisponible);
-				System.out.println("A l'étal n° " + (i + 1) + ", j'achete " + quantiteDisponible + " " + chaineProduit
-						+ " et je paye " + prix + " sous.");
-				quantiteRestante -= quantiteDisponible;
-			}
-		}
-		String chaineProduit = accorderNomProduit(produit, quantiteSouhaitee);
-		System.out.println("Je voulais " + quantiteSouhaitee + " " + chaineProduit + ", j'en ai acheté "
-				+ (quantiteSouhaitee - quantiteRestante) + ".");
-	}
+//	public static void acheterProduit(Etal[] marche, String produit, int quantiteSouhaitee) {
+//		int quantiteRestante = quantiteSouhaitee;
+//		for (int i = 0; i < marche.length && quantiteRestante != 0; i++) {
+//			Etal etal = marche[i];
+//			int quantiteDisponible = etal.contientProduit(produit, quantiteRestante);
+//			if (quantiteDisponible != 0) {
+//				int prix = etal.acheterProduit(quantiteDisponible);
+//				String chaineProduit = accorderNomProduit(produit, quantiteDisponible);
+//				System.out.println("A l'étal n° " + (i + 1) + ", j'achete " + quantiteDisponible + " " + chaineProduit
+//						+ " et je paye " + prix + " sous.");
+//				quantiteRestante -= quantiteDisponible;
+//			}
+//		}
+//		String chaineProduit = accorderNomProduit(produit, quantiteSouhaitee);
+//		System.out.println("Je voulais " + quantiteSouhaitee + " " + chaineProduit + ", j'en ai acheté "
+//				+ (quantiteSouhaitee - quantiteRestante) + ".");
+//	}
 
 	private static String accorderNomProduit(String produit, int quantiteSouhaitee) {
 		String chaineProduit = produit;
