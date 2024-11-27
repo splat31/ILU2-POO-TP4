f8697b877ce2cc2cbf13b20552cac6dd373a7c2f
chaudet <christelle.chaudet@irit.fr>
2024-11-27 11:33:41 +0100

    Update IVillage.java

--- src/villagegaulois/IVillage.java
+++ src/villagegaulois/IVillage.java
@@ -3,7 +3,7 @@ package villagegaulois;
 import personnages.Gaulois;
 
 public interface IVillage {
-	public <P extends Produit> boolean installerVendeur(Etal<P> etal, Gaulois vendeur, P[] produit, int prix);
+	<P extends Produit> boolean installerVendeur(Etal<P> etal, Gaulois vendeur, P[] produit, int prix);
 
-	public void acheterProduit(String produit, int quantiteSouhaitee);
+	void acheterProduit(String produit, int quantiteSouhaitee);
 }
